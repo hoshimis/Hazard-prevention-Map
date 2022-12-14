6bc21aadd6d59070f49cabf7d753d523e1f0d998
taiyo8901 <it212098@trident.ac.jp>
2022-12-14 11:57:37 +0900

    add form

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,85 @@
-const functions = require("firebase-functions");
+/**
+ * @author 小池将弘
+ * @see https://qiita.com/ozaki25/items/565c889a9941a5bbdd76
+ * @see https://tech.chakapoko.com/nodejs/express/params.html
+ */
+
 // expressのimport
-const express = require("express");
+const bodyParser = require('body-parser')
+const express = require('express')
+const admin = require('firebase-admin')
+// const functions = require('firebase-functions')
+const ServiceAccount = require('./ServiceAccount.json')
 
 // expressのappを作成する。
-const app = express();
-// timestampのパスにgetでアクセスしたら以下を実行
-app.get("/timestamp", (request, response) => {
-  //   response.send("Hello from Firebase!");
-  response.send(`${Date.now()}`);
-});
+const app = express()
+app.use(bodyParser.json())
 
+// firestore firebase initialize
+admin.initializeApp({ credential: admin.credential.cert(ServiceAccount) })
+const db = admin.firestore()
+
+// dbの参照取得
+// const docref = db.collection('users').doc('aturing')
+const docref = db.collection('users')
+// helloを返すだけ
+app.get('/hello', (request, response) => {
+  response.send('Hello')
+})
+
+// timestampのパスにgetでアクセスしたら以下を実行
 // コンテンツキャッシュの作成
-app.get("/timestamp", (request, response) => {
-  response.set("Cache-Control", "public", "max-age=300", "smax-age=600");
-  response.send(`${Date.now()}`);
-});
+app.get('/timestamp', (request, response) => {
+  response.set('Cache-Control', 'public', 'max-age=300', 'smax-age=600')
+  response.send(`${Date.now()}`)
+})
+
+app.get('/messages', async (request, response) => {
+  // messageコレクションのデータを全量取得
+  const snapshots = await docref.get()
+  // レスポンスからデータ部分のみ取り出す
+  const messages = snapshots.docs.map((doc) => {
+    doc.data()
+  })
+  response.send(messages)
+})
+
+app.post('/messages', async (request, response) => {
+  console.log('test')
+  const message = {
+    text: request.body.text,
+    timestamp: admin.firestore.FieldValue.serverTimestamp()
+  }
+  await docref.add(message)
+  response.send(message)
+})
+
+// setするデータ
+// const setAda = docref.set({
+//   first: 'Ada',
+//   last: 'Lovelace',
+//   born: '1815'
+// })
+// const setAda = docref.set({
+//   first: 'Alan',
+//   Middle: 'Mathison',
+//   last: 'Turing',
+//   born: '1815'
+// })
+// データの取得
+// db.collection('users')
+//   .get()
+//   .then((snapshot) => {
+//     snapshot.forEach((doc) => {
+//       console.log(`${doc.id} => ${doc.data()}`)
+//     })
+//   })
+//   .catch((err) => {
+//     console.log(`Error getting docments ${err}`)
+//   })
 
 // https関数の作成
 // これらの関数にリクエストが来るたびに処理を返す
-exports.app = functions.https.onRequest(app);
+// exports.app = functions.https.onRequest(app)
+const port = '8080'
+app.listen(port, () => console.log(`app start listening on port ${port}`))
--- public/src/js/map.js
+++ public/src/js/map.js
@@ -10,26 +10,66 @@
  */
 
 var initMap = () => {
-  console.log("init map");
-  const latLng = new google.maps.LatLng(35.170778, 136.882494);
+  console.log('init map')
+  const latLng = new google.maps.LatLng(35.170778, 136.882494)
   const options = {
     zoom: 17, //地図の縮尺値を設定する
-    center: latLng, //地図の中心座標を設定する
-  };
-  const map = new google.maps.Map(document.getElementById("map"), options);
+    center: latLng //地図の中心座標を設定する
+  }
+  const map = new google.maps.Map(document.getElementById('map'), options)
 
   const circle = new google.maps.Circle({
     map: map,
     center: latLng,
-    radius: 90,
-  });
+    radius: 90
+  })
 
   const marker = new google.maps.Marker({
     map: map,
     position: latLng,
-    title: "名古屋駅！！",
-  });
-};
+    title: '名古屋駅！！',
+    class: 'fff/'
+  })
+
+  // クリックイベントを追加
+  map.addListener('click', function (e) {
+    console.log(e.latLng.lat())
+    console.log(e.latLng.lng())
+    getAddress(e.latLng)
+    var lat = document.getElementById('lat')
+    var lng = document.getElementById('lng')
+
+    lat.value = e.latLng.lat();
+    lng.value = e.latLng.lng();
+  })
+}
+
+function getAddress(latlng) {
+  // ジオコーダのコンストラクタ
+  var geocoder = new google.maps.Geocoder()
+  let address
+  
+  // geocodeリクエストを実行。
+  // 第１引数はGeocoderRequest。緯度経度⇒住所の変換時はlatLngプロパティを入れればOK。
+  // 第２引数はコールバック関数。
+  geocoder.geocode(
+    {
+      latLng: latlng
+    },
+    function (results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        // results.length > 1 で返ってくる場合もありますが・・・。
+        if (results[0].geometry) {
+          // 住所を取得(日本の場合だけ「日本, 」を削除)
+          address = results[0].formatted_address.replace(/^日本, /, '')
+          const place = document.getElementById('place')
+          place.value = address
+        }
+      }
+    }
+  )
+
+}
 
 /**
  * @author 小池将弘
@@ -46,16 +86,16 @@ var initMap = () => {
  * @see https://www.koreyome.com/web/json-data-get/
  */
 const getJSON = () => {
-  const req = new XMLHttpRequest();
+  const req = new XMLHttpRequest()
   req.onreadystatechange = () => {
     if (req.readyState == 4 && req.status == 200) {
-      const data = JSON.parse(req.responseText);
-      const latlng = new google.maps.LatLng(35.6811673, 139.7670516);
+      const data = JSON.parse(req.responseText)
+      const latlng = new google.maps.LatLng(35.6811673, 139.7670516)
       const options = {
         zoom: 15, //地図の縮尺値を設定する
-        center: latlng, //地図の中心座標を設定する
-      };
-      const map = new google.maps.Map(document.getElementById("map"), options);
+        center: latlng //地図の中心座標を設定する
+      }
+      const map = new google.maps.Map(document.getElementById('map'), options)
 
       // JSON のデータ数分処理
       for (let i = 0; i < data.marker.length; i++) {
@@ -64,39 +104,39 @@ const getJSON = () => {
           lng: ${data.marker[i].lng},
           name: ${data.marker[i].name},
           flg: ${data.marker[i].flg}
-          `);
-        const lat = data.marker[i].lat; //緯度
-        const lng = data.marker[i].lng; //経度
-        const name = data.marker[i].name; //タイトル
-        const flg = data.marker[i].flg; //flg
+          `)
+        const lat = data.marker[i].lat //緯度
+        const lng = data.marker[i].lng //経度
+        const name = data.marker[i].name //タイトル
+        const flg = data.marker[i].flg //flg
 
         // flgがAの値のみを表示する
-        if (flg === "A") {
+        if (flg === 'A') {
           //マーカーとタイトルを表示する
           const marker = new google.maps.Marker({
             map: map, //表示している地図を指定する
             position: new google.maps.LatLng(lat, lng), //マーカーの表示位置を設定する
             title: name, //タイトルに値を設定する
-            icon: "http://mt.google.com/vt/icon?psize=16&font=fonts/Roboto-Regular.ttf&color=ff330000&name=icons/spotlight/spotlight-waypoint-b.png&ax=44&ay=48&scale=3&text=A",
-          });
-        } else if (flg === "B") {
+            icon: 'http://mt.google.com/vt/icon?psize=16&font=fonts/Roboto-Regular.ttf&color=ff330000&name=icons/spotlight/spotlight-waypoint-b.png&ax=44&ay=48&scale=3&text=A'
+          })
+        } else if (flg === 'B') {
           const marker = new google.maps.Marker({
             map: map, //表示している地図を指定する
             position: new google.maps.LatLng(lat, lng), //マーカーの表示位置を設定する
             title: name, //タイトルに値を設定する
-            icon: "http://mt.google.com/vt/icon?psize=16&font=fonts/Roboto-Regular.ttf&color=ff330000&name=icons/spotlight/spotlight-waypoint-b.png&ax=44&ay=48&scale=3&text=B",
-          });
-        } else if (flg === "C") {
+            icon: 'http://mt.google.com/vt/icon?psize=16&font=fonts/Roboto-Regular.ttf&color=ff330000&name=icons/spotlight/spotlight-waypoint-b.png&ax=44&ay=48&scale=3&text=B'
+          })
+        } else if (flg === 'C') {
           const marker = new google.maps.Marker({
             map: map, //表示している地図を指定する
             position: new google.maps.LatLng(lat, lng), //マーカーの表示位置を設定する
             title: name, //タイトルに値を設定する
-            icon: "http://mt.google.com/vt/icon?psize=16&font=fonts/Roboto-Regular.ttf&color=f3331111&name=icons/spotlight/spotlight-waypoint-b.png&ax=44&ay=48&scale=3&text=C",
-          });
+            icon: 'http://mt.google.com/vt/icon?psize=16&font=fonts/Roboto-Regular.ttf&color=f3331111&name=icons/spotlight/spotlight-waypoint-b.png&ax=44&ay=48&scale=3&text=C'
+          })
         }
       }
     }
-  };
-  req.open("GET", "./data.json", true);
-  req.send();
-};
+  }
+  req.open('GET', './data.json', true)
+  req.send()
+}
