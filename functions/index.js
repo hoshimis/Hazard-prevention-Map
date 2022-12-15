2ca2464b701b311e2a933a77f7bb4d10f617aab4
koike.masahiro <koike.engineer@gmail.com>
2022-12-16 08:56:49 +0900

    add templatePage

--- functions/index.js
+++ functions/index.js
@@ -5,24 +5,33 @@
  */
 
 // expressのimport
-const bodyParser = require('body-parser')
-const { request, response } = require('express')
 const express = require('express')
+const bodyParser = require('body-parser')
 const admin = require('firebase-admin')
 const functions = require('firebase-functions')
 const ServiceAccount = require('./ServiceAccount.json')
+const engines = require('consolidate')
 
-// expressのappを作成する。
+// expressのappを作成
 const app = express()
+app.engine('hbs', engines.handlebars)
+app.set('views', './views')
+app.set('view engine', 'hbs')
 app.use(bodyParser.json())
 
-// firestore firebase initialize
+// firebase firestore の初期化
 admin.initializeApp({ credential: admin.credential.cert(ServiceAccount) })
 const db = admin.firestore()
 
 // dbの参照取得
 const docref = db.collection('userpost')
 
+// app.get('/aa', (request, response) => {
+//   response.set('Cache-Control', 'public, max-age=300, s-maxage=600')
+//   response.render('index', { "test" })
+// })
+
+// ユーザ投稿をするAPI
 app.post('/userpost', async (request, response) => {
   const message = {
     // text: request.body.text,
@@ -33,6 +42,7 @@ app.post('/userpost', async (request, response) => {
   response.redirect('/')
 })
 
+// ユーザ投稿を取得するAPI
 app.get('/', async (request, response) => {
   docref
     .get()
